2aaa50f4bdf13d39d4263af370c8263e1a4f65c4
98kimjh <98kimjh@gmail.com>
2021-04-03 19:37:34 +0900

    Create SubActivity & Move via Intent

--- app/src/main/java/com/example/capstonetest/MainActivity.java
+++ app/src/main/java/com/example/capstonetest/MainActivity.java
@@ -23,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         btn_test = findViewById(R.id.btn_test);
         btn_move = findViewById(R.id.btn_move);
-        str = editText.getText().toString();
 
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                str = editText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("str", str);
                 startActivity(intent);  //  activity 이동
