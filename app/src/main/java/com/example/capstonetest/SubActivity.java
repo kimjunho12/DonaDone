fcaa05c72883eb2b2091f035b4359b0f052167a0
98kimjh <98kimjh@gmail.com>
2021-04-03 20:30:48 +0900

    Create ListView

--- app/src/main/java/com/example/capstonetest/SubActivity.java
+++ app/src/main/java/com/example/capstonetest/SubActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubActivity extends AppCompatActivity {
+
+    private ListView list;
 
     private TextView tv_sub;
     @Override
@@ -15,6 +22,15 @@ public class SubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sub);
 
         tv_sub = findViewById(R.id.tv_sub);
+        list = (ListView)findViewById(R.id.list);
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("Hello World!!!!!!!!!!!");
+        data.add("Android!!");
+        adapter.notifyDataSetChanged(); // 현재 추가 상황 저장
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("str");
